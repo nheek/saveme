f5e633fb6a5ad21e33bff38cc8a743d3ee78aa10
griphaugland <griphaugland@gmail.com>
2023-09-06 01:16:33 +0200

    Initial Commit, Site structure, fonts, Tailwind installation, started build

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'display': ['Bowlby One'],
+      'body': ['Share Tech Mono'],
+    },
+    fontSize: {
+      'xxs': ['12px'],
+      'xs': ['14px'],
+      's': ['16px'],
+      'm': ['20px'],
+      'l': ['24px'],
+      'xl': ['28px'],
+      'xxl': ['32px'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
