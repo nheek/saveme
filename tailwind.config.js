bc3677e89fef025a1e3321cde709cb7904d48eb7
griphaugland <griphaugland@gmail.com>
2023-09-06 01:42:20 +0200

    Added slight responsivenessimprovements

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'xxxxs': '300px',
+      'xxxs': '350px',
+      'xxs': '400px',
+      'xxs': '500px',
+      'xs': '580px',
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     fontFamily: {
       'display': ['Bowlby One'],
       'body': ['Share Tech Mono'],
