4dd2013d00c4502c81e03e2cae18f38764795215
griphaugland <griphaugland@gmail.com>
2023-09-07 00:52:30 +0200

    Written down logic for functionality build

---
+++ src/script.js
@@ -0,0 +1,18 @@
+/*
+KNAPP LOGIKK
+1. Counter for mengden clicks
+2. Funksjon som tar mengden clicks x innsats og gir ut et objekt 
+som inneholder denne infoen.
+3. Funksjon som tar infoen om clicks og innsats og videresender til 
+bank --> som gjennomfører transaksjonen.
+
+POPUP
+Funksjon for om det er første gang det brukes knappen.
+Om JA - lager Sparekonto med navn Sparemål navn under Forklaring
+OM Nei - gjemmer lag konto biten under Forklaring
+
+KNAPPFUNKSJONALITET
+Endre Innsatsmengde ved opp og ned knapp + ved å regne ut nærmeste
+5er fra tallet som skrives inn
+
+*/
