22eb6ec5abc955fb0475f490675e105b1d37c27e
griphaugland <griphaugland@gmail.com>
2023-09-09 16:13:12 +0200

    Added button functionality, started logic for click x value calculation

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
 ### stacc-challenge
+
 This is my submission for the Stacc challenge
 
 ## heads-up
-On this project I have focused on making this function for mobile devices with a min-with of 320px to a max-width of 550px. 
+
+On this project I have focused on making this function for mobile devices with a min-with of 320px to a max-width of 550px.
 The reasoning for this is that the application is created to be used in a banking app.
 
 ## explanation
-The idea behind the app is to give users a good way to save money when having the urge to buy something. Buying on impulse is something that most people do once in a while. With Impulsknappen added as a widget or just using it in the app, users can feel good about themselves by clicking the button every time there is an urge to buy something. 
-People like to feel good about their choices. By giving them the ability to send the money they were to spend on an impulse buy, to a locked bank account in a single click, they will learn to save and value the money they have. 
 
-## how-does-it-work
-The value in each click can be decided by the user and is standard 5NOK and maximum 5000NOK. 
-One click starts the function. After the button registers 2 minutes of inactivity, a function will multiply the amount of clicks by the input value and give a total sum. 
+The idea behind the app is to give users a good way to save money when having the urge to buy something. Buying on impulse is something that most people do once in a while. With Impulsknappen added as a widget or just using it in the app, users can feel good about themselves by clicking the button every time there is an urge to buy something.
+People like to feel good about their choices. By giving them the ability to send the money they were to spend on an impulse buy, to a locked bank account in a single click, they will learn to save and value the money they have.
+
+## how-it-works
+
+The value in each click can be decided by the user and is standard 5NOK and maximum 5000NOK.
+One click starts the function. After the button registers 2 minutes of inactivity, a function will multiply the amount of clicks by the input value and give a total sum.
 This sum will be charged directly to the users bank-account and the money will be sent to a separate account with the name of the users saving goal (sparemål).
 The user also gets to decide if they want to lock the account for a certain amount of time, or if they wish to leave the account open.
 The standard here will be to lock the saving goal account for 2 months. A pop-up will however ask the user for this information the first time it is used.
--- script.js
+++ script.js
@@ -11,9 +11,95 @@ Funksjon for om det er første gang det brukes knappen.
 Om JA - lager Sparekonto med navn Sparemål navn under Forklaring
 OM Nei - gjemmer lag konto biten under Forklaring
 
+clickSession = {
+    inputAmount: 5,
+    inputClicks: 1,
+    
+}
+
 KNAPPFUNKSJONALITET
 Endre Innsatsmengde ved opp og ned knapp + ved å regne ut nærmeste
 5er fra tallet som skrives inn
 
 */
+const impulseBtn = document.getElementById('impulsknappen');
+const downBtn = document.getElementById('arrowDown');
+const upBtn = document.getElementById('arrowUp');
+const countDisplay = document.getElementById('click');
+let displayInputValue = document.getElementById('inputValue');
+const localStorageInputValue = localStorage.getItem('amountPerClick');
+let inputValueChoices = [5, 10, 20, 50, 100,];
 
+user = {
+    standard: {
+        inputValue: 5,
+    },
+    info: {
+        userName: "testuser",
+        id: 0,
+        inputValue: localStorageInputValue,
+    }
+}
+
+
+    if(localStorageInputValue == 5)
+    {
+    currentInputValue = 5;
+    }
+    else if(localStorageInputValue == 10) {
+    currentInputValue = 10;
+    }
+    else if(localStorageInputValue == 20) {
+     currentInputValue = 20;
+    }
+    else if(localStorageInputValue == 50) {
+    currentInputValue = 50;
+    }
+    else if(localStorageInputValue == 100) {
+     currentInputValue = 100;
+    }
+    else{
+    localStorage.setItem('amountPerClick', 5)
+    }
+displayInputValue.innerHTML = `${currentInputValue}`;
+let index = inputValueChoices.indexOf(currentInputValue)
+
+function moveInputValue(advance) {
+    index = (index + (advance ? 1 : -1) + inputValueChoices.length) % inputValueChoices.length;
+    currentInputValue = inputValueChoices[index]
+    localStorage.setItem('amountPerClick', `${currentInputValue}`);
+    displayInputValue.innerHTML = `${currentInputValue}`;
+    console.log(`Index: ${index}`)
+console.log(`currentInputValue: ${currentInputValue}`)
+ }
+
+upBtn.addEventListener('click', () => {
+ let advance = true;
+ moveInputValue(advance);
+})
+downBtn.addEventListener('click', () => {
+    let advance = false;
+    moveInputValue(advance);
+}) 
+
+let count = 0;
+impulseBtn.addEventListener('click', () => {
+    clickSession()
+})
+
+function clickSession() {
+    let clickSessionObj = [
+        {clicks: count,
+        clickValue: currentInputValue
+    }
+    ];
+    function clickCounter() {
+        count++
+        countDisplay.innerHTML = `${count * currentInputValue} `;
+        clickSessionObj.clicks = count;
+        clickSessionObj.clickValue = currentInputValue;
+        return clickSessionObj
+     }
+     clickCounter();
+}
+/* Clicksession -> hver gang man endrer innsats blir clicksession avsluttet og addert til ny klikk sessionStorage. */
