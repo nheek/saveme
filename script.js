861241ef971c306ad68d141a3ab0e6fd1a379175
griphaugland <griphaugland@gmail.com>
2023-09-14 12:04:01 +0200

    Fixed totalvalue update functionality

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ const toBeCharged = document.getElementById('toBeCharged');
 const displayInputValue = document.getElementById('inputValue');
 const localStorageInputValue = localStorage.getItem('amountPerClick');
 let inputValueChoices = [5, 10, 20, 50, 100,];
+const savingsGoal = localStorage.getItem('savings-goal');
+const savingGoalInput = document.getElementById('saving-target')
+const savingGoalBtn = document.querySelector('.saving-target-button');
+const lockTime = localStorage.getItem('savings-goal-locked');
+const popUpElement = document.querySelector('.popUp');
 
 if(localStorageInputValue == 5)
 {
@@ -62,6 +67,7 @@ function sumAllClicks() {
     }
     countDisplay.innerText = `${totalCost}`;
     toBeCharged.innerHTML = `Nå har du spart ${totalCost}kr på impulskjøp :) Kjempe bra!`;
+    sessionStorage.setItem('SavedInSession', totalCost)
     return totalCost
 }
 let totalCost = 0;
@@ -80,14 +86,80 @@ function ani() {
         document.getElementById('click').classList.remove('numAni');
         document.getElementById('click').style.opacity = "0";
     }
-    setTimeout(remAni, 500)
+    setTimeout(remAni, 600)
   }
 
-//        Plans for sending amount to bank for money transfer
+ function setSavedAmount() {
+    let previousSaved = Number(localStorage.getItem('totalSaved'));
+    let currentSessionSaved = Number(sessionStorage.getItem('SavedInSession'));
+    let totalSaved =  previousSaved + currentSessionSaved
+    for (let i = 0; i < clickSessionArr.length; i++) {
+        totalSaved += clickSessionArr[i]
+    }
+    countDisplay.innerText = `${totalSaved}`;
+    toBeCharged.innerHTML = `Nå har du spart ${totalSaved}kr på impulskjøp :) Kjempe bra!`;
+    sessionStorage.setItem('SavedInSession', totalSaved)
+    localStorage.setItem('totalSaved', totalSaved);
+    return totalCost
+  }
 
-/*   A function that measures inactivity after 5 minutes and/or when the app is closed, will send 
-  the totalCost value to be charged to the customers account
-  
-  function registerInactivity() {
-   
-  } */
+  savingGoalBtn.addEventListener('click', ()=> {
+    let savingGoal = savingGoalInput.value;
+    let savingGoalLocked = Number(document.querySelector('#saving-target-lock').value)
+    localStorage.setItem('savings-goal', savingGoal)
+    localStorage.setItem('savings-goal-locked', savingGoalLocked)
+    showSavingsAccount();
+})
+
+let user = {
+    preferredInputValue: localStorageInputValue,
+    account: {
+        sparemål: localStorage.getItem('savings-goal'),
+        accountNr: 10203040506,
+        locked: lockTime,
+    },
+    totalSaved: localStorage.getItem('totalSaved'),
+}
+
+function showSavingsAccount() {
+   document.querySelector('.total-saved').innerText = `${user.totalSaved}NOK`
+   document.querySelector('.savings-goal-account').innerText = `${user.account.sparemål}`
+   document.querySelector('.savings-goal-account-nr').innerText = `${user.account.accountNr}`
+   document.querySelector('.total-saved-timeLock').innerText = `Låst(${lockTime}mnd)`
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('click', (event) => {
+        if (event.target.classList.contains('endre')) {
+            localStorage.removeItem('savings-goal-locked')
+            localStorage.removeItem('savings-goal')
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+              });
+              popUp();
+        }
+    });
+});
+function checkAccount() {
+    if(savingsGoal !== null) {
+        closePopUp();
+    } else{
+        popUp()
+    }
+}
+checkAccount()
+function popUp() {
+    popUpElement.style.display = "flex";
+    document.querySelector('.account-change-container').style.display = "flex";
+}
+function closePopUp(){
+    popUpElement.style.display = "none";
+    document.querySelector('.account-change-container').style.display = "none";
+    showSavingsAccount()
+}
+window.addEventListener('beforeunload', () => {
+   alert("Sending last data to localstorage, bye bye:)")
+        setSavedAmount();
+        sessionStorage.setItem('SavedInSession', 0)
+});
