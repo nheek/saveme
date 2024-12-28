b84e2acc05bce064fdb31939b7c3e3c3a965a944
Nick <83167470+nheek@users.noreply.github.com>
2024-12-28 18:40:59 +0100

    implemented prettier

--- script.js
+++ script.js
@@ -1,178 +1,174 @@
-const impulseBtn = document.getElementById('impulsknappen');
-const downBtn = document.getElementById('arrowDown');
-const upBtn = document.getElementById('arrowUp');
-const countDisplay = document.getElementById('click');
-const toBeCharged = document.getElementById('toBeCharged');
-const displayInputValue = document.getElementById('inputValue');
-let localStorageInputValue = localStorage.getItem('amountPerClick') || 5;
-let inputValueChoices = [5, 10, 20, 50, 100,];
-const savingsGoal = localStorage.getItem('savings-goal');
-const savingGoalInput = document.getElementById('saving-target')
-const savingGoalBtn = document.querySelector('.saving-target-button');
-const lockTime = localStorage.getItem('savings-goal-locked');
-const popUpElement = document.querySelector('.popUp');
-const form = document.querySelector('form');
-const closePopUpbtn = document.querySelector('.popUpClose')
+const impulseBtn = document.getElementById("impulsknappen");
+const downBtn = document.getElementById("arrowDown");
+const upBtn = document.getElementById("arrowUp");
+const countDisplay = document.getElementById("click");
+const toBeCharged = document.getElementById("toBeCharged");
+const displayInputValue = document.getElementById("inputValue");
+let localStorageInputValue = localStorage.getItem("amountPerClick") || 5;
+let inputValueChoices = [5, 10, 20, 50, 100];
+const savingsGoal = localStorage.getItem("savings-goal");
+const savingGoalInput = document.getElementById("saving-target");
+const savingGoalBtn = document.querySelector(".saving-target-button");
+const lockTime = localStorage.getItem("savings-goal-locked");
+const popUpElement = document.querySelector(".popUp");
+const form = document.querySelector("form");
+const closePopUpbtn = document.querySelector(".popUpClose");
 
-if(localStorageInputValue == 5)
-{
-currentInputValue = 5;
-}
-else if(localStorageInputValue == 10) {
-currentInputValue = 10;
-}
-else if(localStorageInputValue == 20) {
-currentInputValue = 20;
-}
-else if(localStorageInputValue == 50) {
-currentInputValue = 50;
-}
-else if(localStorageInputValue == 100) {
-currentInputValue = 100;
-}
-else{
-localStorage.setItem('amountPerClick', 5)
+if (localStorageInputValue == 5) {
+  currentInputValue = 5;
+} else if (localStorageInputValue == 10) {
+  currentInputValue = 10;
+} else if (localStorageInputValue == 20) {
+  currentInputValue = 20;
+} else if (localStorageInputValue == 50) {
+  currentInputValue = 50;
+} else if (localStorageInputValue == 100) {
+  currentInputValue = 100;
+} else {
+  localStorage.setItem("amountPerClick", 5);
 }
 
 displayInputValue.innerHTML = `${currentInputValue}`;
-let index = inputValueChoices.indexOf(currentInputValue)
+let index = inputValueChoices.indexOf(currentInputValue);
 
 function moveInputValue(advance) {
-    index = (index + (advance ? 1 : -1) + inputValueChoices.length) % inputValueChoices.length;
-    currentInputValue = inputValueChoices[index]
-    localStorage.setItem('amountPerClick', `${currentInputValue}`);
-    displayInputValue.innerHTML = `${currentInputValue}`;
- }
+  index =
+    (index + (advance ? 1 : -1) + inputValueChoices.length) %
+    inputValueChoices.length;
+  currentInputValue = inputValueChoices[index];
+  localStorage.setItem("amountPerClick", `${currentInputValue}`);
+  displayInputValue.innerHTML = `${currentInputValue}`;
+}
 
-upBtn.addEventListener('click', () => {
- let advance = true;
- moveInputValue(advance);
-})
-downBtn.addEventListener('click', () => {
-    let advance = false;
-    moveInputValue(advance);
-}) 
+upBtn.addEventListener("click", () => {
+  let advance = true;
+  moveInputValue(advance);
+});
+downBtn.addEventListener("click", () => {
+  let advance = false;
+  moveInputValue(advance);
+});
 
 let count = 1;
-impulseBtn.addEventListener('click', () => {
-    ani()
-    clickSession()
-})
+impulseBtn.addEventListener("click", () => {
+  ani();
+  clickSession();
+});
 
-let clickSessionArr = [
- ];
+let clickSessionArr = [];
 
 function sumAllClicks() {
-    let totalCost = 0;
-    for (let i = 0; i < clickSessionArr.length; i++) {
-        totalCost += clickSessionArr[i]
-    }
-    countDisplay.innerText = `${totalCost}`;
-    toBeCharged.innerHTML = `Nå har du spart ${totalCost}kr på impulskjøp :) Kjempe bra!`;
-    sessionStorage.setItem('SavedInSession', totalCost)
-    return totalCost
+  let totalCost = 0;
+  for (let i = 0; i < clickSessionArr.length; i++) {
+    totalCost += clickSessionArr[i];
+  }
+  countDisplay.innerText = `${totalCost}`;
+  toBeCharged.innerHTML = `Nå har du spart ${totalCost}kr på impulskjøp :) Kjempe bra!`;
+  sessionStorage.setItem("SavedInSession", totalCost);
+  return totalCost;
 }
 let totalCost = 0;
 function clickSession() {
-    function clickCounter() {
-        count++
-     }
-     clickCounter();
-    clickSessionArr.push(currentInputValue);
-    sumAllClicks()
+  function clickCounter() {
+    count++;
+  }
+  clickCounter();
+  clickSessionArr.push(currentInputValue);
+  sumAllClicks();
 }
 
 function ani() {
-    document.getElementById('click').classList.add('numAni');
-    const remAni = () => {
-        document.getElementById('click').classList.remove('numAni');
-        document.getElementById('click').style.opacity = "0";
-    }
-    setTimeout(remAni, 600)
-  }
-
- function setSavedAmount() {
-    let previousSaved = Number(localStorage.getItem('totalSaved'));
-    let currentSessionSaved = Number(sessionStorage.getItem('SavedInSession'));
-    let totalSaved =  previousSaved + currentSessionSaved
-    countDisplay.innerText = `${totalSaved}`;
-    toBeCharged.innerHTML = `Nå har du spart ${totalSaved}kr på impulskjøp :) Kjempe bra!`;
-    sessionStorage.setItem('SavedInSession', totalSaved)
-    localStorage.setItem('totalSaved', totalSaved);
-    return totalSaved
-  }
-
-  form.addEventListener('submit', (event)=> {
-    let savingGoal = savingGoalInput.value;
-    let savingGoalLocked = Number(document.querySelector('#saving-target-lock').value)
-    localStorage.setItem('savings-goal', savingGoal)
-    localStorage.setItem('savings-goal-locked', savingGoalLocked)
-    showSavingsAccount();
-}) 
-
-
-let user = {
-    preferredInputValue: localStorageInputValue,
-    account: {
-        sparemål: localStorage.getItem('savings-goal'),
-        accountNr: 10203040506,
-        locked: lockTime,
-    },
-    totalSaved: localStorage.getItem('totalSaved'),
+  document.getElementById("click").classList.add("numAni");
+  const remAni = () => {
+    document.getElementById("click").classList.remove("numAni");
+    document.getElementById("click").style.opacity = "0";
+  };
+  setTimeout(remAni, 600);
 }
 
+function setSavedAmount() {
+  let previousSaved = Number(localStorage.getItem("totalSaved"));
+  let currentSessionSaved = Number(sessionStorage.getItem("SavedInSession"));
+  let totalSaved = previousSaved + currentSessionSaved;
+  countDisplay.innerText = `${totalSaved}`;
+  toBeCharged.innerHTML = `Nå har du spart ${totalSaved}kr på impulskjøp :) Kjempe bra!`;
+  sessionStorage.setItem("SavedInSession", totalSaved);
+  localStorage.setItem("totalSaved", totalSaved);
+  return totalSaved;
+}
+
+form.addEventListener("submit", (event) => {
+  let savingGoal = savingGoalInput.value;
+  let savingGoalLocked = Number(
+    document.querySelector("#saving-target-lock").value,
+  );
+  localStorage.setItem("savings-goal", savingGoal);
+  localStorage.setItem("savings-goal-locked", savingGoalLocked);
+  showSavingsAccount();
+});
+
+let user = {
+  preferredInputValue: localStorageInputValue,
+  account: {
+    sparemål: localStorage.getItem("savings-goal"),
+    accountNr: 10203040506,
+    locked: lockTime,
+  },
+  totalSaved: localStorage.getItem("totalSaved"),
+};
+
 function showSavingsAccount() {
-   document.querySelector('.total-saved').innerText = `${user.totalSaved}NOK`
-   document.querySelector('.savings-goal-account').innerText = `${user.account.sparemål}`
-   document.querySelector('.total-saved-timeLock').innerText = `Låst(${lockTime}mnd)`
+  document.querySelector(".total-saved").innerText = `${user.totalSaved}NOK`;
+  document.querySelector(".savings-goal-account").innerText =
+    `${user.account.sparemål}`;
+  document.querySelector(".total-saved-timeLock").innerText =
+    `Låst(${lockTime}mnd)`;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-document.addEventListener('click', (event) => {
-        if (event.target.classList.contains('endre')) {
-            window.scrollTo({
-                top: 0,
-                behavior: "smooth",
-              });
-              closePopUpbtn.style.display = "block";
-              popUp();
-        }
-    });
+  document.addEventListener("click", (event) => {
+    if (event.target.classList.contains("endre")) {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+      closePopUpbtn.style.display = "block";
+      popUp();
+    }
+  });
 });
 function checkAccount() {
-    if(savingsGoal !== null) {
-        closePopUp();
-    }
-    else if(savingsGoal == null){
-        popUp()
-        closePopUpbtn.style.display = "none";
-    }
-    else {
-        popUp()
-    }
+  if (savingsGoal !== null) {
+    closePopUp();
+  } else if (savingsGoal == null) {
+    popUp();
+    closePopUpbtn.style.display = "none";
+  } else {
+    popUp();
+  }
 }
-checkAccount()
+checkAccount();
 function popUp() {
-    popUpElement.style.display = "flex";
-    document.querySelector('.account-change-container').style.display = "flex";
+  popUpElement.style.display = "flex";
+  document.querySelector(".account-change-container").style.display = "flex";
 }
-function closePopUp(){
-    popUpElement.style.display = "none";
-    document.querySelector('.account-change-container').style.display = "none";
-    showSavingsAccount()
+function closePopUp() {
+  popUpElement.style.display = "none";
+  document.querySelector(".account-change-container").style.display = "none";
+  showSavingsAccount();
 }
-window.addEventListener('beforeunload', () => {
-   alert("Sending last data to localstorage, bye bye:)")
-        setSavedAmount();
-        sessionStorage.setItem('SavedInSession', 0)
+window.addEventListener("beforeunload", () => {
+  alert("Sending last data to localstorage, bye bye:)");
+  setSavedAmount();
+  sessionStorage.setItem("SavedInSession", 0);
 });
-window.addEventListener('pagehide', ()=> {
-    alert("Sending last data to localstorage, bye bye:)")
-    setSavedAmount();
-    sessionStorage.setItem('SavedInSession', 0)
-})
-window.addEventListener('unload', ()=> {
-    alert("Sending last data to localstorage, bye bye:)")
-    setSavedAmount();
-    sessionStorage.setItem('SavedInSession', 0)
-})
+window.addEventListener("pagehide", () => {
+  alert("Sending last data to localstorage, bye bye:)");
+  setSavedAmount();
+  sessionStorage.setItem("SavedInSession", 0);
+});
+window.addEventListener("unload", () => {
+  alert("Sending last data to localstorage, bye bye:)");
+  setSavedAmount();
+  sessionStorage.setItem("SavedInSession", 0);
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,41 +3,40 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     screens: {
-      'xxxxs': '300px',
-      'xxxs': '350px',
-      'xxs': '400px',
-      'xxs': '500px',
-      'xs': '580px',
-      'sm': '640px',
+      xxxxs: "300px",
+      xxxs: "350px",
+      xxs: "400px",
+      xxs: "500px",
+      xs: "580px",
+      sm: "640px",
       // => @media (min-width: 640px) { ... }
 
-      'md': '768px',
+      md: "768px",
       // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
+      lg: "1024px",
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
+      xl: "1280px",
       // => @media (min-width: 1280px) { ... }
 
-      '2xl': '1536px',
+      "2xl": "1536px",
       // => @media (min-width: 1536px) { ... }
     },
     fontFamily: {
-      'display': ['Bowlby One'],
-      'body': ['Share Tech Mono'],
+      display: ["Bowlby One"],
+      body: ["Share Tech Mono"],
     },
     fontSize: {
-      'xxs': ['12px'],
-      'xs': ['14px'],
-      's': ['16px'],
-      'm': ['20px'],
-      'l': ['24px'],
-      'xl': ['28px'],
-      'xxl': ['32px'],
+      xxs: ["12px"],
+      xs: ["14px"],
+      s: ["16px"],
+      m: ["20px"],
+      l: ["24px"],
+      xl: ["28px"],
+      xxl: ["32px"],
     },
     extend: {},
   },
   plugins: [],
-}
-
+};
